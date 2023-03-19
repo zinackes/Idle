3ea8b6067c95b9b2f1108cda41ae54c11c994aa3
zinackes <zinacke@gmail.com>
2023-03-19 23:36:33 +0100

    New money generator system, + new animations

--- main.js
+++ main.js
@@ -13,6 +13,9 @@ const prestige_title = document.getElementById("animation_1");
 const prestige_desc = document.getElementById("animation_2");
 const prestige_value = document.getElementById("animation_3");
 const prestige_button = document.getElementById("animation_4");
+const prestige_indication = document.getElementById("animation_5");
+const prestige_button_2 = document.getElementById("animation_6");
+const moneypersec_animation = document.getElementById("moneypersecond");
 const money_info = document.querySelector(".money_info")
 var money = 0;
 var Tick = 1000;
@@ -63,6 +66,7 @@ UpCostsMulti[2] = 1.15;
 UpCostsMulti[3] = 1.22;
 
 var upgrades;
+var money_gen_indi = [0, 0, 0, 0];
 let i = 0;
 
 
@@ -89,15 +93,16 @@ function SwitchBuyMode(){
 
 buymode_text.textContent = "x" + 1;
 
-function Money_Generator(){
-  const initial = 0;
-    upgrades = Ups.reduce((accumulator, currentValue) => accumulator + currentValue,
-    initial);
-    money_gen = upgrades * moneybooster;
-    money += money_gen;
+function Money_Generator_Individual(){
+  for(let i = 0;i< IndividualBoost.length;i++){
+    money_gen_indi[i] = Ups[i]*((IndividualBoost[i]*+GlobalBoost)*moneybooster);
+    money+= money_gen_indi[i];
     formatMoney(money);
     Update();
+  }
 }
+
+
 
 function formatMoney(value) {
   for (let i = moneyNotations.length - 1; i >= 0; i--) {
@@ -111,37 +116,50 @@ function formatMoney(value) {
 
 function Update(){
     money_text.textContent = formatMoney(money);
-    money_per_sec_text.textContent = formatMoney(money_gen);
+    for(let i = 0;i< money_gen_indi.length;i++){
+      money_per_sec = money_gen_indi.reduce((accumulator, currentValue) => accumulator + currentValue);
+      money_per_sec_text.textContent = formatMoney(money_per_sec);
+    }
     xp_lvl_text.textContent = xp_lvl;
-    Xp_display(); 
+    Xp_display();
 }
 
 
 setInterval(Update, 500); 
-setInterval(Money_Generator, Tick);
+setInterval(Money_Generator_Individual, 1000);
 
 async function Prestige_class_open(){
+  moneypersec_animation.classList.toggle("animate__zoomOut");
   prestige_box.classList.toggle("active");
   prestige_info.classList.toggle("prestige_active");
   prestige_title.classList.toggle("animation_1");
   prestige_desc.classList.toggle("animation_2");
   prestige_value.classList.toggle("animation_3");
   prestige_button.classList.toggle("animation_4");
+  prestige_indication.classList.toggle("animation_5");
+  prestige_button_2.classList.toggle("animation_6");
   money_info.setAttribute("onclick", "Prestige_class_close()");
-  antimatter_affichage = money/1;
+  antimatter_affichage = money*0.000001;
   antimatter_text.textContent = formatMoney(antimatter);
   antimatter_after_text.textContent = formatMoney(antimatter_affichage);
+  console.log(antimatter);
+  console.log(antimatter_affichage);
 
+  await delay(500);
+  moneypersec_animation.classList.toggle("moneypersecond_active");
   
-  await delay(2000);
+  
+  await delay(1500);
+  moneypersec_animation.classList.toggle("animate__zoomOut");
+  moneypersec_animation.classList.toggle("animate__zoomIn");
   prestige_title.classList.toggle("animation_1");
-
-  await delay(500)
   prestige_desc.classList.toggle("animation_2");
-  await delay(500)
   prestige_value.classList.toggle("animation_3");
-  await delay(5000)
   prestige_button.classList.toggle("animation_4");
+  prestige_indication.classList.toggle("animation_5");
+  prestige_button_2.classList.toggle("animation_6");
+  await delay(1000);
+  moneypersec_animation.classList.toggle("animate__zoomIn");
 }
 
 async function Prestige_class_close(){
@@ -151,14 +169,28 @@ async function Prestige_class_close(){
   prestige_desc.classList.toggle("animation_not_2");
   prestige_value.classList.toggle("animation_not_3");
   prestige_button.classList.toggle("animation_not_4");
+  prestige_indication.classList.toggle("animation_not_5");
+  prestige_button_2.classList.toggle("animation_not_6");
+  moneypersec_animation.classList.toggle("animate__zoomOut");
   money_info.setAttribute("onclick", "Prestige_class_open()");
 
+  
   await delay(500);
+  moneypersec_animation.classList.toggle("moneypersecond_active");
   prestige_title.classList.toggle("animation_not_1");
   prestige_desc.classList.toggle("animation_not_2");
   prestige_value.classList.toggle("animation_not_3");
   prestige_button.classList.toggle("animation_not_4");
+  prestige_indication.classList.toggle("animation_not_5");
+  prestige_button_2.classList.toggle("animation_not_6");
   prestige_info.classList.toggle("prestige_active");
+
+  await delay(900);
+  moneypersec_animation.classList.toggle("animate__zoomOut");
+  moneypersec_animation.classList.toggle("animate__zoomIn");
+
+  await delay(1000);
+  moneypersec_animation.classList.toggle("animate__zoomIn");
 
 
 
@@ -184,10 +216,13 @@ function Reset(){
   upCosts[1] = 100;
   upCosts[2] = 1000;
   upCosts[3] = 5000;
+  milestone_number = [0,0,0,0];
   xp = 0;
   maxxp = 10;
   xp_lvl = 0;
   moneybooster = 1;
+  GlobalBoost=1;
+  IndividualBoost = [1,1,1,1];
 }function PrestigeReset(){
   money = 0;
   Ups[0] = 1;
